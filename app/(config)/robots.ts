b9799fb10345fb835aa0ec0524cecb600555f0e4
Bebe Jane <bjornthief@gmail.com>
2025-08-23 00:23:50 +0200

    rovbots

--- app/(config)/robots.ts
+++ app/(config)/robots.ts
@@ -4,8 +4,7 @@ export default function robots(): MetadataRoute.Robots {
 	return {
 		rules: {
 			userAgent: '*',
-			allow: '/',
-			disallow: '/en/',
+			disallow: '/',
 		},
 		sitemap: `${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`,
 	};
