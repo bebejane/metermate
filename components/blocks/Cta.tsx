ba38b11e26e9b207265b39805873b71128c8abf1
Bebe Jane <bjornthief@gmail.com>
2025-08-25 10:10:34 +0200

    video block

--- components/blocks/Cta.tsx
+++ components/blocks/Cta.tsx
@@ -1,4 +1,4 @@
-import s from './Cta2.module.scss';
+import s from './Cta.module.scss';
 import Content from '@/components/common/Content';
 import cn from 'classnames';
 
@@ -6,6 +6,6 @@ export type CTAProps = {
 	data: CtaRecord;
 };
 
-export default function Cta2({ data: { text } }: CTAProps) {
+export default function Cta({ data: { text } }: CTAProps) {
 	return <section className={cn(s.cta)}>{text && <Content content={text} />}</section>;
 }
---
+++ components/blocks/Video.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import s from './Video.module.scss';
+import { VideoPlayer } from 'next-dato-utils/components';
+
+export type VideoProps = {
+	data: VideoRecord;
+};
+export default function Video({ data }: VideoProps) {
+	if (!data || !data.video) return null;
+	console.log(data);
+	return (
+		<div className={s.wrap}>
+			<VideoPlayer data={data.video} className={s.video} />
+			<figcaption>{data.caption}</figcaption>
+		</div>
+	);
+}
