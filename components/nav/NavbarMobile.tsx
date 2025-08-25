3281cce7f07316ad46f5e4444f4dd1904f52a48b
Bebe Jane <bjornthief@gmail.com>
2025-08-25 15:09:57 +0200

    mobile menu

--- components/nav/NavbarMobile.tsx
+++ components/nav/NavbarMobile.tsx
@@ -3,10 +3,13 @@
 import s from './NavbarMobile.module.scss';
 import cn from 'classnames';
 import { usePathname, useSearchParams } from 'next/navigation';
-import Link from 'next/link';
+import { Link } from '@/i18n/routing';
 import { useEffect, useState } from 'react';
 import { Menu, MenuItem } from '@/lib/menu';
+import { Image } from 'react-datocms';
 import Hamburger from './Hamburger';
+import { useSession } from 'next-auth/react';
+import Content from '@/components/common/Content';
 
 export type NavbarMobileProps = {
 	menu: Menu;
@@ -14,28 +17,41 @@ export type NavbarMobileProps = {
 	allProducts: AllProductsQuery['allProducts'];
 };
 
-export default function NavbarMobile({ menu }: NavbarMobileProps) {
+export default function NavbarMobile({ menu, allProducts, contact }: NavbarMobileProps) {
+	const { status } = useSession();
 	const path = usePathname();
 	const qs = useSearchParams().toString();
 	const pathname = `${path}${qs.length > 0 ? `?${qs}` : ''}`;
 	const [selected, setSelected] = useState<string | null>(null);
 	const [open, setOpen] = useState(false);
-	const member = menu.find(({ id }) => id === 'member');
+	const [sub, setSub] = useState<'contact' | 'products' | null>(null);
+	const top = menu.filter((item) => item.position === 'left' || item.id === 'contact');
+
+	const bottom = menu
+		.filter((item) => item.position === 'right')
+		.map((item) =>
+			item.id === 'login' && status === 'authenticated' ? { ...item, title: 'Medlem', slug: '/medlem' } : item
+		);
+
+	function handleClick(id: string) {
+		setSelected(id);
+		setSub(id === 'contact' ? 'contact' : id === 'products' ? 'products' : null);
+	}
+	function isSelected(item: MenuItem) {
+		if (sub) return sub === item.id;
+		return pathname === item.slug;
+	}
 
 	useEffect(() => {
 		setOpen(false);
 	}, [path, qs]);
-
-	function isSelected(item: MenuItem) {
-		return pathname.startsWith(item.slug) || pathname === item.slug;
-	}
 
 	return (
 		<>
 			<div className={cn(s.topbar, open && s.open)}>
 				<figure className={s.logo}>
 					<Link href={'/'}>
-						<img src={!open ? '/images/logo.svg' : '/images/logo_gray.svg'} alt='Logo' />
+						<img src={'/images/logo.svg'} alt='Logo' />
 					</Link>
 				</figure>
 				<div className={s.hamburger}>
@@ -44,16 +60,38 @@ export default function NavbarMobile({ menu }: NavbarMobileProps) {
 			</div>
 			<nav className={cn(s.navbarMobile, open && s.open)}>
 				<ul className={s.menu}>
-					{menu.map(({ id, title, href, slug, sub }) => (
+					{top.map((item) => (
 						<li
-							key={id}
-							className={cn(sub && s.dropdown, pathname.startsWith(slug) && s.active)}
-							onClick={() => setSelected(selected === id ? null : id)}
+							key={item.id}
+							className={cn(sub && s.dropdown, isSelected(item) && s.active)}
+							onClick={() => handleClick(selected === item.id ? null : item.id)}
 						>
-							{slug ? <Link href={slug}>{title}</Link> : <span>{title}</span>}
+							{item.slug ? <Link href={item.slug}>{item.title}</Link> : <span>{item.title}</span>}
+							{item.id === 'products' && sub === 'products' && (
+								<ul>
+									{allProducts?.map(({ id, title, image, slug }) => (
+										<li key={id}>
+											<Link
+												key={id}
+												href={{
+													pathname: '/produkter/[product]',
+													params: { product: slug },
+												}}
+											>
+												{title}
+											</Link>
+										</li>
+									))}
+								</ul>
+							)}
+							{item.id === 'contact' && sub === 'contact' && <Content content={contact.text} className={s.contact} />}
 						</li>
 					))}
 				</ul>
+				<div className={s.help}>
+					<h3>Behöver du hjälp?</h3>
+					<p>Vi har samlat allt du behöver veta för att få igång din MeterMate-dongel här.</p>
+				</div>
 			</nav>
 		</>
 	);
