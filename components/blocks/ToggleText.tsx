e4b61002a30ff66e94721fdd4e155f728905cc58
Bebe Jane <bjornthief@gmail.com>
2025-09-02 14:30:05 +0200

    toggle text toggle

--- components/blocks/ToggleText.tsx
+++ components/blocks/ToggleText.tsx
@@ -1,17 +1,23 @@
+'use client';
+
 import s from './ToggleText.module.scss';
 import cn from 'classnames';
 import Content from '@/components/common/Content';
 import { Image } from 'react-datocms';
+import { useState } from 'react';
 
 export type ToggleTextProps = {
 	data: ToggleTextRecord;
 };
 
 export default function ToggleText({ data: { headline, text } }: ToggleTextProps) {
+	const [show, setShow] = useState(false);
+
 	return (
 		<section className={s.toggletext}>
-			<h3>{headline}</h3>
-			<Content content={text} />
+			<h3 onClick={() => setShow(!show)}>{headline}</h3>
+			<span className={cn(show && s.open)}>▾</span>
+			{show && <Content content={text} />}
 		</section>
 	);
 }
