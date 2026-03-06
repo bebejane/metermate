1087dd6b70292cefc6a651b332d481b8ee21ec31
Bebe Jane <bjornthief@gmail.com>
2026-03-06 11:24:56 +0100

    visual editing

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,9 +1,9 @@
-type SiteLocale = 'en' | 'se'
+type SiteLocale = 'en' | 'se';
 type LocaleParams<K = string, T = string> = {
-  params: Promise<{ locale: SiteLocale, [key: string]: T }>,
-  searchParams?: any
-}
+	params: Promise<{ locale: SiteLocale; [key: string]: T }>;
+	searchParams?: any;
+};
 type PageProps = {
-  children: React.ReactNode;
-  params: LocaleParams['params'];
+	children: React.ReactNode;
+	params: LocaleParams['params'];
 };
